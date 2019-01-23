7cb4898f564c4a376eeb87d9028af0f9310c6338
vagrant <vagrant@homestead>
2019-01-23 06:28:15 +0000

    author policy

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,9 +6,17 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
 use App\Handlers\ImageUploadHandlers;
+use Illuminate\Auth\Access\AuthorizationException;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['show']
+        ]);
+    }
+
     //
     public function show(User $user)
     {
@@ -17,11 +25,13 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->author($user);
         return view('users.edit', compact('user'));
     }
 
     public function update(UserRequest $request, ImageUploadHandlers $upload, User $user)
     {
+        $this->author($user);
         $data = $request->all();
 
         if ($request->avatar) {
@@ -32,4 +42,12 @@ class UsersController extends Controller
         return redirect()->route('users.show', $user->id)->with('success', '个人资料编辑成功！');
     }
 
+    private function author($user)
+    {
+        try {
+            $this->authorize('update', $user);
+        } catch (AuthorizationException $e){
+            abort(403, '无权访问');
+        }
+    }
 }
